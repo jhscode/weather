4bbcb7a44e156c923a582727418c3985b6ffda14
James Shin <jhscode@gmail.com>
2018-08-21 07:29:26 -0400

    basic search bar container

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Weather App</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
